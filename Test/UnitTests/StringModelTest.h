0262025d8660e73597a74125b018946d7495d689
Max Sherr <wolfink@hotmail.com>
2023-01-10 23:42:40 -0500

    Update StringModel pluck test

--- Test/UnitTests/StringModelTest.h
+++ Test/UnitTests/StringModelTest.h
@@ -37,7 +37,7 @@ public:
 	{
 		beginTest("Test pluck method");
 
-		AudioBuffer<float> buffer(1, spec.maximumBlockSize);
+		AudioBuffer<float> buffer(spec.numChannels, spec.maximumBlockSize);
 		dsp::FFT fft(fft_order);
 		int num_buckets = spec.maximumBlockSize;
 
@@ -48,31 +48,33 @@ public:
 		string->setDecay(0.5);
 		string->setVolume(1.0);
 		string->prepare(spec);
-		for (int _ = 0; _ < 3; _++) {
-			float* data = buffer.getWritePointer(0);
-			buffer.clear();
-			//string->clear();
-			string->pluck(0);
-			for (int _ = 0; _ < 10; _++) {
-				string->process(data, 0, num_buckets);
-			}
-			fft.performFrequencyOnlyForwardTransform(data);
-
-			double max  = 0.0;
-			int arg_max = 0;
-			for (int i = 1; i < num_buckets / 2; i++) {
-				if (data[i] > max) {
-					max = data[i];
-					arg_max = i;
+		LOOP(3) {
+			string->pluck();
+			LOOP(10) {
+				buffer.clear();
+				for (int channel = 0; channel < spec.numChannels; channel++) {
+					auto* data = buffer.getWritePointer(channel);
+					string->process(data, channel, num_buckets);
 				}
 			}
-
-			expectWithinAbsoluteError(arg_max, target_bucket, 1, "Plucked frequency is different than expected");
+			for (int channel = 0; channel < spec.numChannels; channel++) {
+				auto* data = buffer.getWritePointer(channel);
+				fft.performFrequencyOnlyForwardTransform(data);
+				double max = 0.0;
+				int arg_max = 0;
+				for (int i = 1; i < num_buckets / 2; i++) {
+					if (data[i] > max) {
+						max = data[i];
+						arg_max = i;
+					}
+				}
+				expectWithinAbsoluteError(arg_max, target_bucket, 1, "Plucked frequency is different than expected");
+			}
 		}
 	}
 private:
 	const int fft_order         = 12;
-	const dsp::ProcessSpec spec = { 44100.0, uint32(1 << (fft_order + 1)), 1 };
+	const dsp::ProcessSpec spec = { 44100.0, uint32(1 << (fft_order + 1)), 2 };
 	uptr<StringModel> string;
 };
 StringModelTest StringModel_test;
